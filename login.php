c21c8bfa27a0e9d255c5183b5936f424fd18a7af
Hyop <ch.arancibia.r@gmail.com>
2016-11-21 00:41:33 -0300

    Trabaja Pedro ql

---
+++ login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+<link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+<script src="assets/js/loginJS.js" type="text/javascript"></script>
+<link href="assets/css/loginCSS.css" rel="stylesheet" type="text/css"/>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <script src="http://mymaplist.com/js/vendor/TweenLite.min.js"></script>
+        <body>
+            <div class="container">
+                <div class="row vertical-offset-100">
+                    <div class="col-md-4 col-md-offset-4">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">                                
+                                <div class="row-fluid user-row">
+                                    <img src="imagenes/Entel.icon.png" class=" img-responsive" alt="Conxole Admin"/>
+                                </div>
+                            </div>
+                            <div class="panel-body">
+                                <form accept-charset="UTF-8" role="form" class="form-signin">
+                                    <fieldset>
+                                        <label class="panel-login">
+                                            <div class="login_result"></div>
+                                        </label>
+                                        <input class="form-control" placeholder="Username" id="username" type="text">
+                                        <input class="form-control" placeholder="Password" id="password" type="password">
+                                        <br></br>
+                                        <input class="btn btn-lg btn-success btn-block" type="submit" id="login" value="Login »">
+                                    </fieldset>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </body>
+            </div>
+    </body>
+</html>
