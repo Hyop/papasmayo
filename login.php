68c29b0ae3477d45b4d56ed482fb41f5f4ab4caa
Hyop <ch.arancibia.r@gmail.com>
2016-11-27 20:49:42 -0300

    ..

--- login.php
+++ login.php
@@ -1,49 +1,88 @@
+<?php 
+ob_start();
+session_start();
+require_once 'config.php'; 
+?>
+<?php 
+	if( !empty( $_POST )){
+		try {
+			$user_obj = new Cl_User();
+			$data = $user_obj->login( $_POST );
+			if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']){
+				header('Location: home.php');
+			}
+		} catch (Exception $e) {
+			$error = $e->getMessage();
+		}
+	}
+	//print_r($_SESSION);
+	if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']){
+		header('Location: home.php');
+	}
+?>
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
-<link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css"/>
-<script src="assets/js/loginJS.js" type="text/javascript"></script>
-<link href="assets/css/loginCSS.css" rel="stylesheet" type="text/css"/>
-
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <script src="http://mymaplist.com/js/vendor/TweenLite.min.js"></script>
-        <body>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Página de inicio de sesión</title>
+    <link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+    
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/login.css" rel="stylesheet">
+    <link href="css/stylish-portfolio.css" rel="stylesheet" type="text/css"/>
+    
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    
+    <div>
+        <title>Dai Credit</title>
+        <img src="" style="" class="img-responsive img-rounded" alt=""/>
+    </div>
+    
+  </head>
+  <body>
+        <header id="top" class="header">
             <div class="container">
-                <div class="row vertical-offset-100">
-                    <div class="col-md-4 col-md-offset-4">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">                                
-                                <div class="row-fluid user-row">
-                                    <img src="imagenes/Entel.icon.png" class=" img-responsive" alt="Conxole Admin"/>
-                                </div>
-                            </div>
-                            <div class="panel-body">
-                                <form accept-charset="UTF-8" role="form" class="form-signin">
-                                    <fieldset>
-                                        <label class="panel-login">
-                                            <div class="login_result"></div>
-                                        </label>
-                                        <input class="form-control" placeholder="Username" id="username" type="text">
-                                        <input class="form-control" placeholder="Password" id="password" type="password">
-                                        <br></br>
-                                        <input class="btn btn-lg btn-success btn-block" type="submit" id="login" value="Login »">
-                                    </fieldset>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </body>
-            </div>
-    </body>
+		<div class="login-form">
+			<?php require_once 'templates/message.php';?>
+			<div class="form-header">
+				<i class="fa fa-user"></i>
+                                <h2>Eranova Web App</h2>
+                                <small>Vendemos <strong>con estilo</strong>.</small>
+			</div>
+			<form id="login-form" method="post" class="form-signin" role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+				<input name="rut" id="rut" type="rut" class="form-control" placeholder="Rut" autofocus> 
+				<input name="password" id="password" type="password" class="form-control" placeholder="Contraseña"> 
+				<button class="btn btn-block bt-login" type="submit" id="submit_btn" data-loading-text="Iniciando....">Iniciar sesión</button>
+			</form>
+			<div class="form-footer">
+				<div class="row">
+                                    <div class="col-xs-6 col-sm-6 col-md-6">
+					<i class="fa fa-lock"></i>
+					<a href="forget_password.php"> Olvidó su contraseña? </a>
+					
+                                    </div>
+				</div>
+			</div>
+		</div>
+	</div>
+        </header>
+
+	<!-- /container -->
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/login.js"></script>
+  </body>
 </html>
+<?php ob_end_flush(); ?>
